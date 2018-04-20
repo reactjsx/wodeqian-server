4b90f14602157145e15ce5c55834221c9923d4d4
EC2 Default User <ec2-user@ip-172-31-13-186.us-east-2.compute.internal>
2018-04-20 12:44:12 +0000

    Add Access control allow origin

--- router/api.js
+++ router/api.js
@@ -5,8 +5,10 @@ const express = require('express'),
 
 const router = express.Router();
 
-router.use((req, res, next) => {
-  console.log(`There's a request to our server!`);
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
